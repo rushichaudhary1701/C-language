233ae5e1b27a6a77756554c0773306ba256d50e8
RUSHI CHAUDHARY <rushichaudhary2004@gmail.com>
2025-08-04 19:57:35 +0530

    C language

---
+++ function_fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Recursive function to get nth Fibonacci number
+int fibonacci(int n) {
+    if(n == 0)
+        return 0;
+    else if(n == 1)
+        return 1;
+    else
+        return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    int terms;
+    printf("Enter number of terms: ");
+    scanf("%d", &terms);
+
+    printf("Fibonacci Series: ");
+    for(int i = 0; i < terms; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+    printf("\n");
+    return 0;
+}
